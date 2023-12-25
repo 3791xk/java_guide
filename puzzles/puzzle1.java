4039f76d81e2405fdbc0787491c7d59b4c35e688
3791xk <xeniak3791@gmail.com>
2023-12-25 15:41:41 -0500

    Created puzzle 1

---
+++ puzzles/puzzle1.java
@@ -0,0 +1,22 @@
+// Complete each of the three exercises
+
+// For lines 4-7 replace the [FILLIN] with valid types
+[FILLIN] x = 10;
+[FILLIN] y = 2.78;
+int z = 3 + x;
+double w = 0.5 + y;
+
+// For lines 11-16 use each variable type once:
+// bool, int, float, char, double, and long
+[FILLIN] a = 1;
+[FILLIN] b = true;
+[FILLIN] c = 'q';
+[FILLIN] d = 3.14;
+[FILLIN] e = 900000000;
+[FILLIN] f = 3.1415926535f;
+
+// For lines 19-22 replace each with a valid value
+char g = [FILLIN];
+int h = [FILLIN];
+float i = [FILLIN];
+bool j = [FILLIN];
